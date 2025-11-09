046b304909afc1abd854d13fa951c3495d6ef02c
Katherine Raguini <110441766+kcraguini@users.noreply.github.com>
2025-11-08 23:31:28 -0600

    Solved 1426 using hashmap

---
+++ Easy/CountingElements.py
@@ -0,0 +1,20 @@
+from collections import Counter
+from typing import List
+class Solution:
+    def countElements(self, arr: List[int]) -> int:
+        nums = Counter(arr)
+        cnt = 0
+        
+        for i in range(len(arr)):
+            if arr[i] + 1 in nums:
+                cnt += 1
+                
+        return cnt
+    
+s = Solution()
+arr = [1,2,3]
+print(s.countElements(arr))
+arr2 = [1,1,3,3,5,5,7,7]
+print(s.countElements(arr2))
+arr3 = [1,1,2,2]
+print(s.countElements(arr3))
