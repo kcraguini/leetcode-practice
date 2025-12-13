4bfd6ed3290c6804f25f93fd8f88b03e112fa04e
Katherine Raguini <110441766+kcraguini@users.noreply.github.com>
2025-12-12 22:42:34 -0600

    Hashmap used to keep the indexes and mark the difference

---
+++ Medium/minimumCardPickup.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        dic = defaultdict(list)
+        for i in range(len(cards)):
+            dic[cards[i]].append(i)
+            
+        ans = float("inf")
+        for key in dic:
+            arr = dic[key]
+            for i in range(len(arr) - 1):
+                ans = min(ans, arr[i + 1] - arr[i] + 1)
+        
+        return ans if ans < float("inf") else -1
+    
+sol = Solution()
+cards = [3,4,2,3,4,7]
+print(sol.minimumCardPickup(cards))
