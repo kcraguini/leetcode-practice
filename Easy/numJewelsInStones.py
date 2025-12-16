abd7066e9cf595d171770a264743bec30fa0eecc
Katherine Raguini <110441766+kcraguini@users.noreply.github.com>
2025-12-15 18:45:39 -0600

    Used a set as a key to check the string if it is registered as a Jewel

---
+++ Easy/numJewelsInStones.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewel_set = set(jewels)
+        cnt = 0
+
+        for stone in stones:
+            if stone in jewel_set:
+                cnt += 1    
+
+
+        return cnt
+
+sol = Solution()
+jewels = "aA"
+stones = "aAAbbbb"    
+print(sol.numJewelsInStones(jewels, stones))
