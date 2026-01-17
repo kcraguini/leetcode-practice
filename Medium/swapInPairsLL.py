f51d49c3facd7531f5abc056590ad0518297549d
Katherine Raguini <110441766+kcraguini@users.noreply.github.com>
2026-01-16 23:08:43 -0600

    repracticing swap pairs for Linked List

--- Medium/swapInPairsLL.py
+++ Medium/swapInPairsLL.py
@@ -1,5 +1,4 @@
 from typing import Optional
-
 class Node:
     def __init__(self, val):
         self.val = val
@@ -8,25 +7,30 @@ class Node:
         
 class Solution:
     def swapPairs(self, head: Node) -> Node:
-        
         if not head or not head.next:
             return head
-
-        dummy = head.next               
-        prev = None                     
+        dummy = head.next
+        prev = None
+        
+        
         while head and head.next:
             if prev:
-                prev.next = head.next  
-            prev = head                 
+                prev.next = head.next
+            prev = head
+            print(f"Prev {prev.val}")
 
-            next_node = head.next.next  
-            head.next.next = head      
-
-            head.next = next_node       
-            head = next_node            
-
+            next_node = head.next.next
+            print(f"Next_node {next_node.val}")
+            head.next.next = head
+            print(f"Head.next.next {head.next.next.val}")
+            head.next = next_node
+            print(f"Head.next {head.next.val}")
+            head = next_node
+            print(f"Head {head.val}")
+            print()
         return dummy
-                    
+
+        
         
     
 def insert(root, val):
