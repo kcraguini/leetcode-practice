31ba6aabf7d5fd707ca276b22fb7c398184f8535
Katherine Raguini <110441766+kcraguini@users.noreply.github.com>
2026-01-28 23:12:57 -0600

    repracticed validParentheses

--- Easy/validParentheses.py
+++ Easy/validParentheses.py
@@ -7,14 +7,16 @@ class Solution:
             if c in match:
                 stack.append(c)
             else:
-                if not stack:
-                    return False
-                previous_opening = stack.pop()
-                if match[previous_opening] != c:
+                print(f"Not in the stack {c}")
+                if not stack: # If the stack is empty automatically false
                     return False
                 
-        return not stack
-
+                else:
+                    prev_matching = stack.pop()
+                    if match[prev_matching] != c:
+                        return False
+                    
+        return True
 if __name__ == '__main__':
     s = "()[]{}"
     sol = Solution()
