9c8360cd0e98ec4514482981c11a40ba5dad8eba
Katherine Raguini <110441766+kcraguini@users.noreply.github.com>
2026-02-15 00:01:20 -0600

    Updated dailyTemperatures

--- Easy/dailyTemperatures.py
+++ Easy/dailyTemperatures.py
@@ -2,6 +2,18 @@ from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        
+        n = len(temperatures)
+        answer = [0] * n
+
+        for i in range(n):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                j = stack.pop()
+                answer[j] = i - j
+            stack.append(i)
+
+        return answer
         
 
 if __name__ == "__main__":
