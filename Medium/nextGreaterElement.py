dbe4a5b754d49837ee15e8bbd857fd8c293496f1
Katherine Raguini <110441766+kcraguini@users.noreply.github.com>
2026-02-18 23:57:33 -0600

    Setting up for leetcode question 496

---
+++ Medium/nextGreaterElement.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        pass
+
+sol = Solution()
+nums1 = [4,1,2]
+nums2 = [1,3,4,2]
