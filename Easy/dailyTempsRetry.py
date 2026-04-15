5800c601c461ef4c4c040e71cfe13da169249daa
Katherine Raguini <110441766+kcraguini@users.noreply.github.com>
2026-04-14 23:59:20 -0500

    Relearning monotonic data structure

---
+++ Easy/dailyTempsRetry.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        answer = [0] * len(temperatures)
+        
+        for i in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                j = stack.pop()
+                answer[j] = i - j
+            stack.append(i)
+        
+        return answer
+    
+
+sol = Solution()
+print(sol.dailyTemperatures([73,74,75,71,69,72,76,73])) 
+print(sol.dailyTemperatures([40,35,32,37,50]))
