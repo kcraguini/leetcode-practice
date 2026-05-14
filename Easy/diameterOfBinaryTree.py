5022b36340528b3d7cf8ece1f90845f6d746f07f
Katherine Raguini <110441766+kcraguini@users.noreply.github.com>
2026-05-13 23:33:04 -0500

    working on this but im struggling. >:(

--- Easy/diameterOfBinaryTree.py
+++ Easy/diameterOfBinaryTree.py
@@ -8,15 +8,32 @@ class TreeNode:
 
 
 class Solution:
-    def diamterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def helper(root, diameter):
+            if not root:
+                return 0
+            
+            if root.right is None:
+                left = 1 + helper(root.left, diameter)
+            elif root.left is None:
+                right = 1 + helper(root.right, diameter)
+            else:
+                left = 1 + helper(root.left, diameter)
+                right = 1 + helper(root.right, diameter)
+
+            #print(f"Left {left}, Right {right}")
+            diameter += max(left, right)
+
+            return diameter
+        return helper(root, 0)
+    
+    def preOrder(self, root: Optional[TreeNode]):
         if not root:
-            return 0
+            return
         
-        if root.left is None:
-            return 1 + self.diamterOfBinaryTree(root.right)
-        elif root.right is None:
-            return 1 + self.diamterOfBinaryTree(root.left)
-        return 1 + max(self.diamterOfBinaryTree(root.left), self.diamterOfBinaryTree(root.right))
+        print(root.val)
+        self.preOrder(root.left)
+        self.preOrder(root.right)
     
 
 four = TreeNode(4, None, None)
@@ -24,3 +41,11 @@ five = TreeNode(5, None, None)
 two = TreeNode(2, four, five)
 three = TreeNode(3, None, None)
 one = TreeNode(1, two, three) 
+
+sol = Solution()
+print(sol.diameterOfBinaryTree(one))
+#sol.preOrder(one)
+
+otherTwo = TreeNode(2, None, None)
+otherOne = TreeNode(1, otherTwo, None)
+print(sol.diameterOfBinaryTree(otherOne))
